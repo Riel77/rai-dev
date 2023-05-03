f801df077f59675aa362222a8f0e46ffc97df18b
Muhamad Ridwan <muhamad.ridwan7777@gmail.com>
2023-05-03 19:28:55 +0700

    change host

--- src/App.js
+++ src/App.js
@@ -12,62 +12,9 @@ import KontakKami from "./components/KontakKami";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [merchants, setMerchants] = useState(false);
-  useEffect(() => {
-    getMerchant();
-  }, []);
-  function getMerchant() {
-    fetch("https://goldfish-app-jjp9x.ondigitalocean.app")
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        setMerchants(data);
-      });
-  }
-  function createMerchant() {
-    let CountID = prompt("Enter CountID");
-    let Count = prompt("Enter count");
-    fetch("https://goldfish-app-jjp9x.ondigitalocean.app/merchants", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ CountID, Count }),
-    })
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
-  function deleteMerchant() {
-    let CountID = prompt("Enter merchant CountID");
-    fetch(
-      `https://goldfish-app-jjp9x.ondigitalocean.app/merchants/${CountID}`,
-      {
-        method: "DELETE",
-      }
-    )
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
-
   return (
     <div>
       <HeroComp />
-      {merchants ? merchants : "There is no merchant data available"}
-      <br />
-      <button onClick={createMerchant}>Add merchant</button>
-      <br />
-      <button onClick={deleteMerchant}>Delete merchant</button>
 
       {/* Content */}
       <NavbarComp />
